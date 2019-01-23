1a67c40643aabae40df6dd3a92f0ec6899fc18ee
Laetitia LEJEUNE <llejeune@e3r4p4.42.fr>
2019-01-23 14:17:06 +0100

    ajout

--- fdf.c
+++ fdf.c
@@ -6,12 +6,64 @@
 /*   By: llejeune <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 16:03:20 by llejeune          #+#    #+#             */
-/*   Updated: 2019/01/22 18:58:30 by llejeune         ###   ########.fr       */
+/*   Updated: 2019/01/23 14:16:37 by llejeune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "fdf.h"
+
+t_list	*ft_new_node(t_list *new_node)
+{
+	if (!(new_node = (t_list *)malloc(sizeof(t_list))))
+		return (0);
+	new_node->point.x = 0;
+	new_node->point.y = 0;
+	new_node->point.z = 0;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	ft_add_node(t_list **alst, t_list *new_node)
+{
+	if ((*alst) != NULL && new_node != NULL)
+	{
+		while ((*alst)->next != NULL)
+			(*alst) = (*alst)->next;
+		(*alst)->next = new_node;
+	}
+	else if ((*alst) == NULL)
+	{
+		(*alst) = new_node;
+	}
+}
+
+void	ft_map(int *fd)
+{
+	int		i;
+	int		c;
+	char	**line;
+	char	**tmp;
+	t_list	*new_node;
+
+	if (fd < 0)
+		return ;
+	c = 0;
+	while (get_next_line(*fd, line) > 0)
+	{
+		tmp = ft_strsplit(line, ' ');
+		i = 0;
+		while (*tmp[i] != 0)
+		{
+			new_node = ft_new_node(new_node);
+			new_node->point.x = i;
+			new_node->point.y = c;
+			new_node->point.z = ((int)*tmp)[i] - 48;
+			i++;
+		}
+		c++;
+	}
+}
 
 void	ft_fill_pixel(my_m *m)
 {
@@ -26,10 +78,13 @@ void	ft_fill_pixel(my_m *m)
 	m->str[pixel + 3] = 0;
 }
 
-int		main()
+int		main(int ac, char **av)
 {
 	my_m	m;
+	int		fd;
 
+	ac = 2;
+	fd = (av[1], O_RDONLY);
 	m.mlx_ptr = mlx_init();
 	m.win_ptr = mlx_new_window(m.mlx_ptr, 800, 600, "FdF");
 	m.img_ptr = mlx_new_image(m.mlx_ptr, 800, 600);
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: llejeune <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 17:47:55 by llejeune          #+#    #+#             */
-/*   Updated: 2019/01/22 18:03:57 by llejeune         ###   ########.fr       */
+/*   Updated: 2019/01/23 11:44:22 by llejeune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,30 @@
 # include <mlx.h>
 # include "libft/libft.h"
 
-typedef struct	s
+typedef struct		s_point
 {
-	void		*mlx_ptr;
-	void		*win_ptr;
-	void		*img_ptr;
-	int			bpp;
-	int			s_l;
-	int			endian;
-	char		*str;
-	int			x;
-	int			y;
-}				my_m;
+	int				x;
+	int				y;
+	int				z;
+}					t_point;
+
+typedef struct		s_list
+{
+	t_point			*point;
+	struct s_list	*next;
+}					t_list;
+
+typedef struct		s
+{
+	void			*mlx_ptr;
+	void			*win_ptr;
+	void			*img_ptr;
+	int				bpp;
+	int				s_l;
+	int				endian;
+	char			*str;
+	int				x;
+	int				y;
+}					my_m;
 
 #endif
