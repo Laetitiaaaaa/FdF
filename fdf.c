10cda9468a5f38792281942126850f98ef2807da
Laetitia LEJEUNE <llejeune@e3r3p13.42.fr>
2019-01-24 19:38:25 +0100

    maj

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: llejeune <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 16:03:20 by llejeune          #+#    #+#             */
-/*   Updated: 2019/01/23 20:16:35 by llejeune         ###   ########.fr       */
+/*   Updated: 2019/01/24 19:31:21 by llejeune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 
 t_v3	*ft_new_node(t_v3 *new_node)
 {
-	printf("c\n");
 	if (!(new_node = malloc(sizeof(t_v3))))
 		return (0);
 	new_node->point.x = 0;
@@ -30,103 +29,152 @@ void	ft_add_node(t_v3 **alst, t_v3 *new_node)
 {
 	t_v3	*keep;
 
-	printf("b\n");
 	keep = (*alst);
-	printf("ahhhhh\n");
 	if (keep != NULL && new_node != NULL)
 	{
-		printf("non\n");
 		while (keep->next != NULL)
 			keep = keep->next;
 		keep->next = new_node;
 	}
 	if ((*alst) == NULL)
-	{
-		printf("arg\n");
 		(*alst) = new_node;
-	}
-	printf("bla\n");
 }
 
-void	ft_map(int *fd, t_v3 **lst_point, my_m *m)
+void	ft_map(int *fd, my_m *m)
 {
 	char	*line;
 	char	**tmp;
 	t_v3	*new_node;
 
-	printf("d\n");
 	if (!(line = (char *)malloc(sizeof(char) * 100)))
 		return ;
 	if (fd < 0)
 		return ;
-	printf("d\n");
 	m->c = 0;
 	while (get_next_line(*fd, &line) > 0)
 	{
 		tmp = ft_strsplit(line, ' ');
 		m->i = 0;
-		while (*tmp[m->i] != 0)
+		while (tmp[m->i] != 0)
 		{
 			new_node = ft_new_node(new_node);
 			new_node->point.x = m->i;
 			new_node->point.y = m->c;
 			new_node->point.z = ft_atoi(tmp[m->i]);
-			ft_add_node(lst_point, new_node);
+			ft_add_node(&m->lst_point, new_node);
 			m->i++;
 		}
 		m->c++;
 	}
 }
 
-void	ft_fill_pixel(my_m *m, int x, int y)
+void	ft_fill_pixel_black(my_m *m, float x, float y)
 {
 	int		pixel;
 
 	pixel = 4 * (x + (800 * y));
 	m->str[pixel] = 0;
 	m->str[pixel + 1] = 0;
-	m->str[pixel + 2] = 0xff;
+	m->str[pixel + 2] = 0;
 	m->str[pixel + 3] = 0;
+}
+
+void	ft_fill_pixel(my_m *m, float x, float y)
+{
+	int		pixel;
+
+	pixel = 4 * (x + (800 * y));
+	m->str[pixel] = 0;
+	m->str[pixel + 1] = 255;
+	m->str[pixel + 2] = 0;
+	m->str[pixel + 3] = 0;
+}
+
+void	ft_always(my_m *m)
+{
+	ft_black_image(&m->lst_point, m);
+	ft_fill_image(&m->lst_point, m);
+	mlx_put_image_to_window(m->mlx_ptr, m->win_ptr, m->img_ptr, 400, 300);
+}
+
+void	ft_black_image(t_v3 **alst, my_m *m)
+{
+	t_v3	*keep;
+
+	keep = (*alst);
+	if (keep != NULL)
+	{
+		while (keep != NULL)
+		{
+			printf("bla\n");
+			ft_fill_pixel_black(m, keep->save.x, keep->save.y);
+			keep = keep->next;
+		}
+	}
 }
 
 void	ft_fill_image(t_v3 **alst, my_m *m)
 {
-	while ((*alst) != NULL)
+	t_v3	*keep;
+
+	keep = (*alst);
+	if (keep != NULL)
 	{
-		printf("a\n");
-		ft_fill_pixel(m, (*alst)->point.x, (*alst)->point.y);
-		(*alst) = (*alst)->next;
+		while (keep != NULL)
+		{
+			printf("bla\n");
+			ft_fill_pixel(m, keep->point.x, keep->point.y);
+			keep = keep->next;
+		}
 	}
+}
+
+void	ft_zoom(t_v3 **lst_point, my_m *m, float i)
+{
+	t_v3 *keep;
+
+	keep = (*lst_point);
+	while (keep != NULL)
+	{
+		keep->save.x = keep->point.x;
+		keep->save.y = keep->point.y;
+		keep->save.z = keep->point.z;
+		keep->point.x = keep->point.x * i;
+		keep->point.y = keep->point.y * i;
+		keep->point.z = keep->point.z * i;
+		keep = keep->next;
+	}
+	ft_always(m);
+}
+
+int		ft_key(int key, my_m *m)
+{
+	if (key == F_H)
+		ft_zoom(&m->lst_point, m, 2);
+	if (key == F_B)
+		ft_zoom(&m->lst_point, m, 0.5);
+	if (key == ESC)
+		exit(0);
+	return (1);
 }
 
 int		main(int ac, char **av)
 {
 	my_m	m;
 	int		fd;
-	t_v3	**lst_point;
 
-	(void)av;
 	ac = 2;
-	lst_point = NULL;
-	printf("A\n");
+	m.lst_point = NULL;
 	fd = open(av[1], O_RDONLY);
-	printf("B\n");
 	m.mlx_ptr = mlx_init();
-	printf("C\n");
 	m.win_ptr = mlx_new_window(m.mlx_ptr, 800, 600, "FdF");
-	printf("D\n");
 	m.img_ptr = mlx_new_image(m.mlx_ptr, 800, 600);
-	printf("E\n");
 	m.str = mlx_get_data_addr(m.img_ptr, &(m.bpp), &(m.s_l), &(m.endian));
-	printf("F\n");
-	ft_map(&fd, lst_point, &m);
-	printf("F\n");
-	ft_fill_image(lst_point, &m);
-	printf("G\n");
+	ft_map(&fd, &m);
+	ft_fill_image(&m.lst_point, &m);
 //	ft_fill_pixel(&m, 400, 300);
-	mlx_put_image_to_window(m.mlx_ptr, m.win_ptr, m.img_ptr, 0, 0);
-	printf("H\n");
+	mlx_put_image_to_window(m.mlx_ptr, m.win_ptr, m.img_ptr, 400, 300);
+	mlx_key_hook(m.win_ptr, ft_key, &m);
 	mlx_loop(m.mlx_ptr);
-	printf("I\n");
 	return (0);
 }
--- fdf.h
+++ fdf.h
@@ -6,12 +6,17 @@
 /*   By: llejeune <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 17:47:55 by llejeune          #+#    #+#             */
-/*   Updated: 2019/01/23 19:27:09 by llejeune         ###   ########.fr       */
+/*   Updated: 2019/01/24 19:12:20 by llejeune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+# define ESC 53
+# define F_H 126
+# define F_B 125
+# define F_G 123
+# define F_D 124
 
 # include <mlx.h>
 # include "libft/libft.h"
@@ -26,6 +31,7 @@ typedef struct		s_point
 typedef struct		s_liste
 {
 	t_point			point;
+	t_point			save;
 	struct s_liste	*next;
 }					t_v3;
 
@@ -40,12 +46,18 @@ typedef struct		s
 	char			*str;
 	int				i;
 	int				c;
+	t_v3			*lst_point;
 }					my_m;
 
 t_v3				*ft_new_node(t_v3 *new_node);
 void				ft_add_node(t_v3 **alst, t_v3 *new_node);
-void				ft_map(int *fd, t_v3 **lst_point, my_m *m);
-void				ft_fill_pixel(my_m *m, int x, int y);
+void				ft_map(int *fd, my_m *m);
+void				ft_fill_pixel(my_m *m, float x, float y);
 void				ft_fill_image(t_v3 **alst, my_m *m);
+int					ft_key(int key, my_m *m);
+void				ft_zoom(t_v3 **lst_point, my_m *m, float i);
+void				ft_always(my_m *m);
+void				ft_black_image(t_v3 **alst, my_m *m);
+void				ft_fill_pixel_black(my_m *m, float x, float y);
 
 #endif
